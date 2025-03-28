daa3a264102d27ba8fdce8c2ccbc379383bc5111
Eva Kerrigan <evakerrigan@gmail.com>
2025-03-29 00:09:20 +0300

    feat: add routing

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="min-h-screen p-8 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-xl">Страница не найдена</p>
+    </main>
+  );
+}
