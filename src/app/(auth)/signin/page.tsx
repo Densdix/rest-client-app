16cd8801404854021fb57399eda46a3adb43dbf1
Densdix <dixfun333333@gmail.com>
2025-04-12 23:49:11 +0300

    feat: authorization with supabase

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,20 @@
-import { SignIn } from '@/components/SignIn';
+import { login } from './actions';
 import Link from 'next/link';
 
 export default function SignInPage() {
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-2xl font-bold">Вход</h1>
-      <SignIn />
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+      </form>
 
       <p>
-        <Link href="/signup">Регистрация</Link>
+        <Link href="/signup">Еще не зарегистрированы? Регистрация.</Link>
       </p>
     </main>
   );
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,14 +1,20 @@
-import { SignUp } from '@/components/SignUp';
+import { signup } from './actions';
 import Link from 'next/link';
 
 export default function SignUpPage() {
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-2xl font-bold">Регистрация</h1>
-      <SignUp />
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={signup}>Sign up</button>
+      </form>
 
       <p>
-        <Link href="/signin">Вход</Link>
+        <Link href="/signin">Уже есть аккаунт? Вход.</Link>
       </p>
     </main>
   );
