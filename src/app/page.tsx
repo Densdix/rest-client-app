37f051d6ecbb3f74fa790907d316a58c7736e404
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-14 02:59:25 +0300

    feat: fix position header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
-'use client';
-
 import Link from 'next/link';
-import { useAppDispatch } from '@/hooks/redux.hook';
-import { logout } from '@/store/authSlice';
-
-import { useAuth } from '@/hooks/auth.hook';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-  const { isAuth } = useAuth();
-
   return (
-    <main className="min-h-screen flex flex-col justify-between  bg-gray-100 dark:bg-gray-900">
-      <Header />
-      {isAuth ? (
-        <div>
-          <div className="w-100 h-100 m-auto border border-sky-500">Наше приложение</div>
-          <button onClick={() => dispatch(logout())}>Разлогиниться</button>
-        </div>
-      ) : (
-        <div className="p-10">
-          <h1 className="text-3xl font-bold">Добро пожаловать!</h1>
+    <>
+      <div className="p-10">
+        <h1 className="text-3xl font-bold">Добро пожаловать!</h1>
 
-          <Link href="/signin">Войти</Link>
-          <Link href="/signup">Регистрация</Link>
-        </div>
-      )}
-      <Footer />
-    </main>
+        <Link href="/signin">Войти</Link>
+        <Link href="/signup">Регистрация</Link>
+      </div>
+    </>
   );
 }
