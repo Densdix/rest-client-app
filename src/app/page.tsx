d366005f0c1da5f24f2b9e845095fd0932e46317
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-14 18:24:03 +0300

    feat: add header-link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <div className="p-10">
-        <h1 className="text-3xl font-bold">Добро пожаловать!</h1>
-
-        <Link href="/signin">Войти</Link>
-        <Link href="/signup">Регистрация</Link>
+      <div className="h-100">
+        <div className="flex flex-col justify-center align-middle items-center">
+          <h1 className="text-3xl font-bold">Добро пожаловать!</h1>
+          <div>
+            <Link href="/signin" className=" text-blue-600 hover:text-blue-700">
+              Войти
+            </Link>
+            <Link href="/signup" className=" text-blue-600 hover:text-blue-700">
+              Регистрация
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,17 @@ export const Header = () => {
     <div className="bg-white dark:bg-gray-800 shadow">
       <div className="p-4 w-full max-w-[1280px] mx-auto flex justify-between  align-middle items-center">
         <div>
-          <Link href="/" className="">
-            <img src="/logo.png" alt="app logo" className="w-22 h-22" />
+          <Link href="/" className=" w-20 h-20 block bg-blue-600 hover:bg-blue-700 rounded-full">
+            {/* <img src="/logo.png" alt="app logo" className="w-22 h-22" /> */}
           </Link>
         </div>
         <div>Lang</div>
-        <div>Войти</div>
-        <div>Регистрация</div>
+        <Link href="/signin" className=" text-blue-600 hover:text-blue-700">
+          Войти
+        </Link>
+        <Link href="/signup" className=" text-blue-600 hover:text-blue-700">
+          Регистрация
+        </Link>
       </div>
     </div>
   );
