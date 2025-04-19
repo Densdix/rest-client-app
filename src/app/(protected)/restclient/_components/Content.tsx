f8bf45e694775985523b4d332dcfae5d6eb813ad
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-19 04:57:15 +0300

    feat: create url right concat

--- src/app/(protected)/restclient/_components/Content.tsx
+++ src/app/(protected)/restclient/_components/Content.tsx
@@ -1,27 +1,19 @@
 'use client';
 
 import React, { useEffect, useRef } from 'react';
-import { useState } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 
 export const Content: React.FC = () => {
-  const [method, setMethod] = useState('GET');
-  const [url, setUrl] = useState('');
-  const [body, setBody] = useState('');
-  // const [response] = useState<unknown | null>(null);
-  const [status] = useState('');
-  const [size] = useState('');
-  const [time] = useState('');
-
   const lastInput = useRef<EventTarget & HTMLInputElement>(null);
 
-  const { register, handleSubmit, control, setValue, getValues } = useForm<{
+  const { register, handleSubmit, control, setValue, getValues, watch } = useForm<{
     url: string;
     method: string;
     body: string;
     paramNames: { name: string; value: string }[];
   }>({
     defaultValues: {
+      url: '',
       paramNames: [{ name: '', value: '' }],
     },
   });
@@ -35,26 +27,36 @@ export const Content: React.FC = () => {
     lastInput.current?.focus();
   }, [fields]);
 
-  function handleAppend(index: number, e: React.ChangeEvent<HTMLInputElement>) {
-    {
-      const currentUrl = getValues('url').includes('?') ? getValues('url').split('?')[0] : getValues('url') + '?';
-      let params = '';
-      const paramNames = getValues('paramNames');
-      paramNames.map((field) => {
-        console.log({ field });
+  useEffect(() => {
+    console.log('сработал watch');
+    createUrl();
+  }, [watch('url'), watch('paramNames')]);
 
-        params += `&${field.name}=${field.value}`;
-        console.log({ name: field.name, value: field.value });
-        // params += concat(field.key, field.value);
-        // console.log(`&${field.key}=${field.value}\n`);
-      });
-      console.log({ final: params, values: paramNames });
-      setValue('url', currentUrl + params);
-      if (fields.length - 1 === index) {
-        append({ name: '', value: '' });
-        lastInput.current = e.target;
-        console.log(lastInput);
-      }
+  function createUrl() {
+    console.log('сработал createUrl');
+    const currentUrl = getValues('url').includes('?') ? getValues('url').split('?')[0] : getValues('url');
+
+    const params = getValues('paramNames')
+      .filter((field) => field.value)
+      .map((field) => {
+        console.log('field: ', field);
+        console.log('field.name: ', field.name, 'field.value: ', field.value);
+        return `${field.name}=${field.value}`;
+      })
+      .join('&');
+
+    const newUrl = params ? `${currentUrl}?${params}` : currentUrl;
+    if (newUrl !== getValues('url')) {
+      setValue('url', newUrl);
+    }
+  }
+
+  function handleAppend(index: number, e: React.ChangeEvent<HTMLInputElement>) {
+    console.log('произошло событие onChange и мы вызвалась функция handleAppend');
+
+    if (fields.length - 1 === index) {
+      append({ name: '', value: '' });
+      lastInput.current = e.target;
     }
   }
 
@@ -63,8 +65,6 @@ export const Content: React.FC = () => {
       <div className="p-4">
         <form onSubmit={handleSubmit((data) => console.log(data))}>
           <div className="flex flex-1 p-4">
-            {/* Левая панель - параметры запроса */}
-
             <div className="w-1/2 pr-2">
               <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-4 mb-4">
                 <div className="flex items-center mb-4">
@@ -72,8 +72,6 @@ export const Content: React.FC = () => {
                     id="method"
                     {...register('method')}
                     className="px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
-                    value={method}
-                    onChange={(e) => setMethod(e.target.value)}
                   >
                     <option>GET</option>
                     <option>POST</option>
@@ -88,8 +86,6 @@ export const Content: React.FC = () => {
                     type="text"
                     placeholder="Enter request URL"
                     className="flex-1 px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
                   />
 
                   <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit">
@@ -115,13 +111,11 @@ export const Content: React.FC = () => {
                           className="border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
                           {...register(`paramNames.${index}.name`)}
                           onChange={(e) => handleAppend(index, e)}
-                          key={field.id + 'name'}
                         />
                         <input
                           className="border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
                           {...register(`paramNames.${index}.value`)}
                           onChange={(e) => handleAppend(index, e)}
-                          key={field.id + 'value'}
                         />
                         <button
                           onClick={(e) => {
@@ -144,21 +138,12 @@ export const Content: React.FC = () => {
                   {...register('body')}
                   className="w-full h-60 px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md bg-white dark:bg-gray-700"
                   placeholder="Enter request body"
-                  value={body}
-                  onChange={(e) => setBody(e.target.value)}
                 ></textarea>
               </div>
             </div>
 
-            {/* Правая панель - ответ */}
             <div className="w-1/2 pl-2">
               <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-4 h-full">
-                <div className="flex justify-between items-center mb-4 text-sm">
-                  <div className="text-green-500 font-medium">Status: {status}</div>
-                  <div className="text-gray-500">Size: {size}</div>
-                  <div className="text-gray-500">Time: {time}</div>
-                </div>
-
                 <div className="border-b dark:border-gray-700 mb-4">
                   <div className="flex space-x-4 mb-2">
                     <button className="px-3 py-1 border-b-2 border-blue-500 font-medium">Response</button>
