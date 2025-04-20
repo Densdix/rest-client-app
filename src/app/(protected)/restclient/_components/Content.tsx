92e3eab84611f6f48f8aa3a8ec2320cbf6a21157
Eva Kerrigan <evakerrigan@gmail.com>
2025-04-20 13:43:11 +0300

    feat: add url block

--- src/app/(protected)/restclient/_components/Content.tsx
+++ src/app/(protected)/restclient/_components/Content.tsx
@@ -10,6 +10,7 @@ import { RequestBodyBlock } from './RequestBodyBlock';
 import { QueryParamsBlock } from './QueryParamsBlock';
 import { HeadersBlock } from './HeadersBlock';
 import { CodeBlock } from './CodeBlock';
+import { UrlBlock } from './UrlBlock';
 
 export interface ContentRequest {
   url: string;
@@ -148,54 +149,13 @@ export const Content: React.FC = () => {
     }
   };
 
-  const getPreviewUrl = () => {
-    const url = getValues('url');
-    if (!url) return url;
-
-    return replaceVariables(url);
-  };
-
   return (
     <div className="p-4">
       <form onSubmit={handleSubmit(handleSubmitRequest)}>
         <div className="flex flex-1 p-4">
           <div className="w-1/2 pr-2">
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-4 mb-4">
-              <div className="flex items-center mb-4">
-                <select
-                  id="method"
-                  {...register('method')}
-                  className="px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
-                >
-                  <option>GET</option>
-                  <option>POST</option>
-                  <option>PUT</option>
-                  <option>PATCH</option>
-                  <option>DELETE</option>
-                </select>
-
-                <input
-                  id="url"
-                  {...register('url')}
-                  type="text"
-                  placeholder="Enter request URL"
-                  className="flex-1 px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md mr-2 bg-white dark:bg-gray-700"
-                />
-
-                <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" type="submit">
-                  Send
-                </button>
-              </div>
-
-              {/* Предпросмотр URL с переменными - только после монтирования */}
-              {isMounted && variables.length > 0 && getValues('url') && (
-                <div className="mb-2 text-sm text-gray-600 dark:text-gray-400">
-                  <div>URL с переменными:</div>
-                  <div className="font-mono bg-gray-100 dark:bg-gray-900 p-1 rounded overflow-x-auto">
-                    {getPreviewUrl()}
-                  </div>
-                </div>
-              )}
+              <UrlBlock register={register} getValues={getValues} isMounted={isMounted} variables={variables} />
             </div>
 
             <QueryParamsBlock
